31413eebbbed347f4bf181fdb69d6f2ff1a07e59
Kevinfd12 <kevinfd12@gmail.com>
2023-07-19 05:59:43 -0400

    init changes

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/kevinfd12/gRPC-Server/invoicer"
+	"google.golang.org/grpc"
+)
+
+type myInvoicerServer struct {
+	invoicer.UnimplementedInvoicerServer
+}
+
+func (s myInvoicerServer) Create(ctx context.Context, req *invoicer.CreateRequest) (*invoicer.CreateResponse, error) {
+	return &invoicer.CreateResponse{
+		Pdf:  []byte(req.From),
+		Docx: []byte("test"),
+	}, nil
+
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8089")
+	if err != nil {
+		log.Fatalf("cannot create listener: %s", err)
+	}
+	serverRegistrar := grpc.NewServer()
+	service := &myInvoicerServer{}
+	invoicer.RegisterInvoicerServer(serverRegistrar, service)
+	err = serverRegistrar.Serve(lis)
+	if err != nil {
+		log.Fatalf("impossible to serve: %s", err)
+	}
+}
